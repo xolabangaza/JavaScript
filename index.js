59230599b35967f85ec2202ab53a0ada9bc59e73
xolabangaza <you@example.com>
2023-05-16 15:42:11 +0200

    JavaScript exercises

---
+++ index.js
@@ -0,0 +1,18 @@
+// variables with string
+let name= "Xola";
+let surname= "Bangaza";
+
+// concat two variable
+let concat= name +" "+ surname;
+console.log(concat)
+
+// declacre variable
+let numberVar= 5;
+console.log( numberVar * 5)
+
+// constant
+const name2= Xola;
+      name2= Bangaza;
+      console.log(name2) 
+    //   I get error when i reasign
+
